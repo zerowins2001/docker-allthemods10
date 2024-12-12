c4a7254359f5cedf6e6f9488f65ce45786ad652c
Roger <rwm810@gmail.com>
2024-12-11 19:32:53 -0500

    update to 2.2

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 
 FROM openjdk:21-buster
 
-LABEL version="2.1"
+LABEL version="2.2"
 
 RUN apt-get update && apt-get install -y curl unzip jq && \
     adduser --uid 99 --gid 100 --home /data --disabled-password minecraft
--- launch.sh
+++ launch.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-NEOFORGE_VERSION=21.1.84
-SERVER_VERSION=2.1
+NEOFORGE_VERSION=21.1.89
+SERVER_VERSION=2.2
 cd /data
 
 if ! [[ "$EULA" = "false" ]]; then
@@ -15,7 +15,7 @@ fi
 
 if ! [[ -f "Server-Files-$SERVER_VERSION.zip" ]]; then
 	rm -fr config defaultconfigs kubejs mods packmenu Simple.zip forge*
-	curl -Lo "Server-Files-$SERVER_VERSION.zip" 'https://edge.forgecdn.net/files/5972/991/Server-Files-2.1.zip' || exit 9
+	curl -Lo "Server-Files-$SERVER_VERSION.zip" 'https://edge.forgecdn.net/files/5983/698/Server-Files-2.2.zip' || exit 9
 	unzip -u -o "Server-Files-$SERVER_VERSION.zip" -d /data
 	DIR_TEST=$(find . -type d -maxdepth 1 | tail -1 | sed 's/^.\{2\}//g')
 	if [[ $(find . -type d -maxdepth 1 | wc -l) -gt 1 ]]; then
