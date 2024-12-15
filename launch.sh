6ead6c7760a1da2aeada5acf9df4eda868298fa6
Roger <rwm810@gmail.com>
2024-12-14 21:19:00 -0500

    update to 2.5

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 set -x
 
 NEOFORGE_VERSION=21.1.89
-SERVER_VERSION=2.4
+SERVER_VERSION=2.5
 cd /data
 
 if ! [[ "$EULA" = "false" ]]; then
@@ -15,7 +15,7 @@ fi
 
 if ! [[ -f "Server-Files-$SERVER_VERSION.zip" ]]; then
 	rm -fr config defaultconfigs kubejs mods packmenu Simple.zip forge*
-	curl -Lo "Server-Files-$SERVER_VERSION.zip" 'https://edge.forgecdn.net/files/5989/403/Server-Files-2.4.zip' || exit 9
+	curl -Lo "Server-Files-$SERVER_VERSION.zip" 'https://edge.forgecdn.net/files/5990/911/Server-Files-2.5.zip' || exit 9
 	unzip -u -o "Server-Files-$SERVER_VERSION.zip" -d /data
 	DIR_TEST=$(find . -type d -maxdepth 1 | tail -1 | sed 's/^.\{2\}//g')
 	if [[ $(find . -type d -maxdepth 1 | wc -l) -gt 1 ]]; then
