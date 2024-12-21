f9c690b66add855078d6fe9ec939b496f059652b
Roger <rwm810@gmail.com>
2024-12-20 22:10:31 -0500

    update to 2.8

--- launch.sh
+++ launch.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+https://www.curseforge.com/minecraft/modpacks/all-the-mods-10/download/6009 060
+
 set -x
 
 NEOFORGE_VERSION=21.1.90
-SERVER_VERSION=2.7
+SERVER_VERSION=2.8
 cd /data
 
 if ! [[ "$EULA" = "false" ]]; then
@@ -15,7 +17,7 @@ fi
 
 if ! [[ -f "Server-Files-$SERVER_VERSION.zip" ]]; then
 	rm -fr config defaultconfigs kubejs mods packmenu Simple.zip forge*
-	curl -Lo "Server-Files-$SERVER_VERSION.zip" 'https://edge.forgecdn.net/files/6006/513/Server-Files-2.7.zip' || exit 9
+	curl -Lo "Server-Files-$SERVER_VERSION.zip" 'https://edge.forgecdn.net/files/6009/060/Server-Files-2.8.zip' || exit 9
 	unzip -u -o "Server-Files-$SERVER_VERSION.zip" -d /data
 	DIR_TEST=$(find . -type d -maxdepth 1 | tail -1 | sed 's/^.\{2\}//g')
 	if [[ $(find . -type d -maxdepth 1 | wc -l) -gt 1 ]]; then
