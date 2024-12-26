7eb2f5705f9afe6dc49598f7036efd200c32cdcf
RFlor14 <57841815+RFlor14@users.noreply.github.com>
2024-12-26 09:49:32 -0600

    Updated to 2.11

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 set -x
 
 NEOFORGE_VERSION=21.1.90
-SERVER_VERSION=2.10
+SERVER_VERSION=2.11
 cd /data
 
 if ! [[ "$EULA" = "false" ]]; then
@@ -15,7 +15,7 @@ fi
 
 if ! [[ -f "Server-Files-$SERVER_VERSION.zip" ]]; then
 	rm -fr config defaultconfigs kubejs mods packmenu Simple.zip forge*
-	curl -Lo "Server-Files-$SERVER_VERSION.zip" 'https://edge.forgecdn.net/files/6017/404/Server-Files-2.10.zip' || exit 9
+	curl -Lo "Server-Files-$SERVER_VERSION.zip" 'https://edge.forgecdn.net/files/6023/933/Server-Files-2.11.zip' || exit 9
 	unzip -u -o "Server-Files-$SERVER_VERSION.zip" -d /data
 	DIR_TEST=$(find . -type d -maxdepth 1 | tail -1 | sed 's/^.\{2\}//g')
 	if [[ $(find . -type d -maxdepth 1 | wc -l) -gt 1 ]]; then
