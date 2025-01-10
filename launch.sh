7845bf031114c27ab0786c72148894e78b129690
r14f <57841815+r14f@users.noreply.github.com>
2025-01-10 13:49:49 -0600

    Updated launch.sh to 2.21

--- launch.sh
+++ launch.sh
@@ -4,7 +4,7 @@ set -x
 
 
 NEOFORGE_VERSION=21.1.93
-SERVER_VERSION=2.20
+SERVER_VERSION=2.21
 cd /data
 
 if ! [[ "$EULA" = "false" ]]; then
@@ -16,7 +16,7 @@ fi
 
 if ! [[ -f "Server-Files-$SERVER_VERSION.zip" ]]; then
 	rm -fr config defaultconfigs kubejs mods packmenu Simple.zip forge*
-	curl -Lo "Server-Files-$SERVER_VERSION.zip" 'https://edge.forgecdn.net/files/6063/217/Server-Files-2.20.zip' || exit 9
+	curl -Lo "Server-Files-$SERVER_VERSION.zip" 'https://edge.forgecdn.net/files/6070/978/Server-Files-2.21.zip' || exit 9
 	unzip -u -o "Server-Files-$SERVER_VERSION.zip" -d /data
 	DIR_TEST=$(find . -type d -maxdepth 1 | tail -1 | sed 's/^.\{2\}//g')
 	if [[ $(find . -type d -maxdepth 1 | wc -l) -gt 1 ]]; then
